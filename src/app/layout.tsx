16a08a8ec376ebb8bd023852ff89e01cd3ff2ecb
Elad Karni <elad.karni@gmail.com>
2024-04-23 16:30:38 -0400

    Test: force device pixel ratio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="HandheldFriendly" content="true" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </Head>
       <body className={inter.className}>
         <Drawer>{children}</Drawer>
